6dbf1b34949d70611fb686bb3bccd3068d4b9b3d
mehdidov <125817777+mehdidov@users.noreply.github.com>
2026-01-08 00:51:41 +0100

    add book migrations and initialize database schema in postgres

---
+++ migrations/001_create_books.up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS books (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    year INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
